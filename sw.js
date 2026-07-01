a28b701e904c6c9733a87312fc5c2082e06a9cb7
LIN4CRE <davidlinacre@hotmail.co.uk>
2026-07-01 20:42:42 +0100

    feat: streaming lab, stop button, pwa, 404, changelog, more tools, nightly cron

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE = "linacre-v1";
+var URLS = ["/", "/index.html", "/manifest.json", "/404.html"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) { return cache.addAll(URLS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
